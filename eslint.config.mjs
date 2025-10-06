5b7d1d4f8549e4932fe1e5e5a285ff525f89e8b7
evgLip <evggenlip@gmail.com>
2025-10-06 20:30:39 +0300

    json-server
    Changes to be committed:
    	modified:   eslint.config.mjs
    	new file:   json-server/db.json
    	new file:   json-server/index.js
    	modified:   package-lock.json
    	modified:   package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,38 +6,37 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import pluginHooks from 'eslint-plugin-react-hooks';
-// import pluginRefresh from 'eslint-plugin-react-refresh';
-// import pluginImport from 'eslint-plugin-import';
 import i18next from 'eslint-plugin-i18next';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [// Этот должно быть здесь в отдельном объекте, чтобы применяться глобально
-{ ignores: ['dist', 'coverage'] }, {
-  files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-  settings: {
-    react: {
-      version: 'detect',
+  { ignores: ['dist', 'coverage'] }, {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
     },
-    // https://github.com/import-js/eslint-import-resolver-typescript#configuration
-    'import/resolver': {
-      typescript: true,
-      node: true,
+    languageOptions: {
+      globals: globals.browser,
     },
   },
-  languageOptions: {
-    globals: globals.browser,
-  },
-}, i18next.configs['flat/recommended'], pluginJs.configs.recommended, // pluginRefresh.configs.recommended,
-pluginReact.configs.flat.recommended, // pluginImport.flatConfigs.recommended,
-// pluginImport.flatConfigs.typescript,
-...tseslint.configs.recommended, {
-  plugins: {
-    'react-hooks': pluginHooks,
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    ...pluginHooks.configs.recommended.rules,
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-  },
-}, ...storybook.configs["flat/recommended"]];
+  i18next.configs['flat/recommended'],
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended, {
+    plugins: {
+      'react-hooks': pluginHooks,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      ...pluginHooks.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-require-imports": "off", //Это правило требует использования синтаксис ES Module import вместо CommonJS require()
+    },
+  }, ...storybook.configs["flat/recommended"]];

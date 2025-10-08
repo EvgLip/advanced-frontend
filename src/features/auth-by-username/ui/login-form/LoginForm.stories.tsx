f9194ae74df2ea35baeedf27c58b3360f8f1b006
evgLip <evggenlip@gmail.com>
2025-10-08 20:37:50 +0300

    added LoginForm.stories
    Changes to be committed:
    	new file:   src/features/auth-by-username/ui/login-form/LoginForm.stories.tsx

---
+++ src/features/auth-by-username/ui/login-form/LoginForm.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+import ThemeDecorator from '@/shared/config/storybook/ThemeDecorator';
+import { ThemeList } from '@/app/providers/theme-provider';
+import LoginForm from '@/features/auth-by-username/ui/login-form/LoginForm';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'reatures/LoginForm',
+  component: LoginForm,
+  decorators: [
+    (Story) =>
+    {
+      return (
+        <ThemeDecorator theme={ThemeList.LIGHT} >
+          <Story />
+        </ThemeDecorator >
+      );
+    },
+  ],
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    //layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+  parameters: {
+    theme: ThemeList.DARK,
+  },
+};

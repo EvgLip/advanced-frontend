491879552e788bc69befd308519106d02debff35
evgLip <evggenlip@gmail.com>
2025-11-10 20:20:01 +0300

    update StoreDecorator and StoreProvider
    Changes to be committed:
    	modified:   src/app/providers/store-provider/ui/StoreProvider.tsx
    	modified:   src/shared/config/storybook/StoreDecorator.tsx

--- src/app/providers/store-provider/ui/StoreProvider.tsx
+++ src/app/providers/store-provider/ui/StoreProvider.tsx
@@ -3,18 +3,27 @@ import { Provider } from 'react-redux';
 
 import { createReduxStore } from '../config/store';
 import { RootState } from '../config/rootState';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
 interface StoreProviderProps
 {
   children: ReactNode;
   initialState?: DeepPartial<RootState>;
+  asyncReducers?: DeepPartial<ReducersMapObject<RootState>>;
 }
 
 export default function StoreProvider(props: StoreProviderProps)
 {
-  const { children, initialState } = props;
+  const {
+    children,
+    initialState,
+    asyncReducers,
+  } = props;
 
-  const store = createReduxStore(initialState as RootState);
+  const store = createReduxStore(
+    initialState as RootState,
+    asyncReducers as ReducersMapObject<RootState>
+  );
 
   return (
     <Provider store={store}>
--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -1,18 +1,33 @@
 import { ReactNode } from 'react';
 import { RootState, StoreProvider } from '@/app/providers/store-provider';
+import { ReducersMapObject } from '@reduxjs/toolkit';
+import { loginReducer } from '@/features/auth-by-username/model/slice/loginSlice';
 
 interface StoryDecoratorProps
 {
   children: ReactNode;
   initialState?: DeepPartial<RootState>;
+  asyncReducers?: DeepPartial<ReducersMapObject<RootState>>;
 }
+
+const defaultAsyncReducer: DeepPartial<ReducersMapObject<RootState>> =
+{
+  login: loginReducer,
+};
 
 export default function StoreDecorator(props: StoryDecoratorProps)
 {
-  const { children, initialState } = props;
+  const {
+    children,
+    initialState,
+    asyncReducers,
+  } = props;
 
   return (
-    <StoreProvider initialState={initialState}>
+    <StoreProvider
+      initialState={initialState}
+      asyncReducers={{ ...defaultAsyncReducer, ...asyncReducers }}
+    >
       {children}
     </StoreProvider >
   );

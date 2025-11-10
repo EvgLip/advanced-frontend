4018e42f6ab929bc5b8ff18422fb0fb5dec04068
evgLip <evggenlip@gmail.com>
2025-11-10 19:17:27 +0300

    исключил из сборки BundleAnalyzerPlugin
    Changes to be committed:
    	modified:   config/build/buildPlugins.ts

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -23,10 +23,10 @@ export function buildPlagins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
       ),
     ];
 
-  plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
   if (isDev)
   {
     plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
   }
 
   return plugins;
